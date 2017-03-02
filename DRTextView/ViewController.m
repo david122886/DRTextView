5e4389267eb01fd106e9a109def84956c80b499a
liudavid <liudavid@liudaviddeMac-mini.local>
2017-03-02 15:19:57 +0800

    测试不是连续区域

--- DRTextView/ViewController.m
+++ DRTextView/ViewController.m
@@ -20,7 +20,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.textView = [[DRTextView alloc] initWithFrame:self.view.bounds withAreaRects:@[NSStringFromCGRect(self.view.bounds)]];
+    CGRect area1 = (CGRect){0,0,CGRectGetWidth(self.view.bounds),300};
+    CGRect area2 = (CGRect){0,500,CGRectGetWidth(self.view.bounds),200};
+    self.textView = [[DRTextView alloc]
+                     initWithFrame:self.view.bounds
+                     withAreaRects:@[NSStringFromCGRect(area1),NSStringFromCGRect(area2)]];
 //    self.textView = [[DRTextView alloc] initWithFrame:self.view.bounds];
     [self.view addSubview:self.textView];
     self.textView.backgroundColor = [UIColor whiteColor];
